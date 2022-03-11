88d3741e0934a30594272eae4d9a476d44bd0856
GiovannaMorais <gigimorais9@hotmail.com>
2022-03-11 16:40:54 -0300

    requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -67,9 +67,24 @@ function catAndMouse(cat1,cat2,mouse) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers){
+  let result=[];
+  for (let index = 0; index < numbers.length; index+=1){
+  if(numbers[index] % 3 === 0 && numbers[index] % 5 !== 0){
+    result.push("fizz")
+  }else if (numbers[index] % 5 === 0 && numbers[index] % 3 !== 0){
+    result.push("buzz")
+  }else if (numbers[index] % 5 === 0  && numbers[index] % 3 === 0 ){
+    result.push("fizzBuzz")
+  }else{
+    result.push("bug!") }
+ }
+    return result
+ 
 }
+
+
+
 
 // Desafio 9
 function encode() {
